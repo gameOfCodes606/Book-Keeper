d3c0cf46b8d52b9c90ae036a069f1a0acc8f5164
Amal Shibu <87291689+AmalShibu888@users.noreply.github.com>
2023-11-02 21:23:23 +0530

    final

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const submitButton = document.getElementById('sub');
+const clearButton = document.getElementsByClassName('clr')[0];
+const orderedList = document.getElementById('cont'); 
+const listItems = document.getElementsByTagName("li")
+const searchInput = document.getElementById('searchBox');
+
+
